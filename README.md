43fabebdae9e3d68ca600b855975c2e6c6027c37
Trevor Hess <trevorhess@Trevors-MacBook-Pro.local>
2017-10-26 10:32:52 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# nv_chef_client
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: nv_chef_client
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
